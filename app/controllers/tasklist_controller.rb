14f899ff6ad6c4de08d3ac1d45cc28bd5554fe6f
kazuaki.matsuo <liveforever.uchinah@gmail.com>
2021-04-11 04:03:11 +0000

    tasklist model

---
+++ app/controllers/tasklist_controller.rb
@@ -0,0 +1,26 @@
+class TasklistController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
+
+
